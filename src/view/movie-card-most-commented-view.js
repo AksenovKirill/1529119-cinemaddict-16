6df4324919941915887982b40797815a0c198415
AksenovKirill <aksenovkirill@yandex.ru>
2021-11-29 00:20:59 +0300

    добавил slice()

--- src/view/movie-card-most-commented-view.js
+++ src/view/movie-card-most-commented-view.js
@@ -5,24 +5,24 @@ export const createMostCommentedFilmCardTepmplate = (data) => {
     return `  
             <p class="film-card__description">${'Sorry, there are currently no discussed films'}</p>`;
   } else {
-    return `<article class="film-card">
+    return  data.slice(0,2).map((card) =>`<article class="film-card">
         <a class="film-card__link">
           <h3 class="film-card__title">Santa Claus Conquers the Martians</h3>
-          <p class="film-card__rating">${data[randomIndex].raiting}</p>
+          <p class="film-card__rating">${card.raiting}</p>
           <p class="film-card__info">
-            <span class="film-card__year">${data[randomIndex].year}</span>
+            <span class="film-card__year">${card.year}</span>
             <span class="film-card__duration">1h 21m</span>
-            <span class="film-card__genre">${data[randomIndex].genre}</span>
+            <span class="film-card__genre">${card.genre}</span>
           </p>
-          <img src="./images/posters/${data[randomIndex].poster}" alt="" class="film-card__poster">
-          <p class="film-card__description">${data[randomIndex].description}</p>
-          <span class="film-card__comments">${data[randomIndex].comments.length} comments</span>
+          <img src="./images/posters/${card.poster}" alt="" class="film-card__poster">
+          <p class="film-card__description">${card.description}</p>
+          <span class="film-card__comments">${card.comments.length} comments</span>
         </a>
         <div class="film-card__controls">
           <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
           <button class="film-card__controls-item film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
           <button class="film-card__controls-item film-card__controls-item--favorite film-card__controls-item--active" type="button">Mark as favorite</button>
         </div>
-      </article>`;
+      </article>`);
   }
 };
--- src/view/movie-card-top-rated-view.js
+++ src/view/movie-card-top-rated-view.js
@@ -6,26 +6,25 @@ export const createTopRatedFilmCardTepmplate = (data) => {
               <p class="film-card__description">${'Sorry, there are no highly rated movies at the moment.'}</p>
             `;
   } else {
-    return `
-          <article class="film-card">
-            <a class="film-card__link">
-              <h3 class="film-card__title">The Man with the Golden Arm</h3>
-              <p class="film-card__rating">${data[randomIndex].raiting}</p>
-              <p class="film-card__info">
-                <span class="film-card__year">${data[randomIndex].year}</span>
-                <span class="film-card__duration">1h 59m</span>
-                <span class="film-card__genre">${data[randomIndex].genre}</span>
-              </p>
-              <img src="./images/posters/${data[randomIndex].poster}" alt="" class="film-card__poster">
-              <p class="film-card__description">${data[randomIndex].description}</p>
-              <span class="film-card__comments">${data[randomIndex].comments.length} comments</span>
-            </a>
-            <div class="film-card__controls">
-              <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
-              <button class="film-card__controls-item film-card__controls-item--mark-as-watched film-card__controls-item--active" type="button">Mark as watched</button>
-              <button class="film-card__controls-item film-card__controls-item--favorite" type="button">Mark as favorite</button>
-            </div>
-          </article>
-      `;
+    return data.slice(0,2).map((card) =>`
+    <article class="film-card">
+      <a class="film-card__link">
+        <h3 class="film-card__title">The Man with the Golden Arm</h3>
+        <p class="film-card__rating">${card.raiting}</p>
+        <p class="film-card__info">
+          <span class="film-card__year">${card.year}</span>
+          <span class="film-card__duration">1h 59m</span>
+          <span class="film-card__genre">${card.genre}</span>
+        </p>
+        <img src="./images/posters/${card.poster}" alt="" class="film-card__poster">
+        <p class="film-card__description">${card.description}</p>
+        <span class="film-card__comments">${card.comments.length} comments</span>
+      </a>
+      <div class="film-card__controls">
+        <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
+        <button class="film-card__controls-item film-card__controls-item--mark-as-watched film-card__controls-item--active" type="button">Mark as watched</button>
+        <button class="film-card__controls-item film-card__controls-item--favorite" type="button">Mark as favorite</button>
+      </div>
+    </article>`);
   }
 };
